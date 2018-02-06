5afc76c9ac0e28bdd257dc82bca517dddcde8ffe
trodrigs1120 <trodrigs@yahoo.com>
2018-02-06 14:54:04 -0500

    1 feature down

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var curl = require('curlrequest');
-var request = curl.request(default_options);
 
-request([options ,] callback);
+
+
 // var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
 
@@ -41,7 +41,35 @@ function SpotifyASong(){
       console.log('response:' , response)
     });
 }
-
+function GetMovieInfo(){
+    var request = require("request");
+    var nodeArgs = process.argv;
+    var movieName = "";
+    if (process.argv[3]!==undefined){
+      movieName = encodeURI(process.argv[3])
+    } else {
+      movieName = "Mr. Nobody"
+      console.log("Hey if you havent seen it, Mr Nobody is on netflix assuming you have that kind of thing ;)")
+    }
+    
+      var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function(error, response, body) {
+      // If the request is successful
+      if (!error && response.statusCode === 200) {
+    
+        // Parse the body of the site and recover just the imdbRating
+        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("Title: " +JSON.parse(body).Title);
+        console.log("Actors: " +JSON.parse(body).Actors);
+        console.log("Plot: " +JSON.parse(body).Plot);
+        console.log("Release Language: " +JSON.parse(body).Language);
+        console.log("Country of origin: " +JSON.parse(body).Country);
+        console.log("Score From " +JSON.parse(body).Ratings[1].Source+" : " +JSON.parse(body).Ratings[1].Value);
+        console.log("IMDB Rating: " +JSON.parse(body).imdbRating);
+      }
+    });
+    }
 
 var argument2 = process.argv[2];
 switch(argument2){
@@ -55,6 +83,7 @@ switch(argument2){
     break;
     case "movie-this":
     console.log("Movie-this")
+    GetMovieInfo()
     break;
     case "do-what-it-says":
     console.log("Do what it says")
---
+++ readme.md
@@ -0,0 +1,11 @@
+Liri bot,
+
+The program will take a few basic arguements,
+
+* `my-tweets` which will print recent tweets from a dummy twitter account.
+
+* `spotify-this-song` + 'song' will output information about the song's artist, its name, a preview link from spotify and the album the song is from.
+
+* `movie-this` + 'MovieTitle' will output information regarding the imdb, rotten tomatoes ratings, country of origin, and other misc information such as actors.
+
+* `do-what-it-says` Should just output spotify this song but for an unspecified track. 
